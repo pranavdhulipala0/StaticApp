993d9bf7f029e4f232e3b6b5d81a0ef8983d69df
Pranav <pranavdhulipala14@gmail.com>
2022-11-28 21:18:18 +0530

    First commit

---
+++ src/main/java/pom/kmit/staticApp/staticApplication/App.java
@@ -0,0 +1,13 @@
+package pom.kmit.staticApp.staticApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
